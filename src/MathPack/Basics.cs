4e81f45384facb9747c07b81798f65edc30d72a8
Christoph Perger <christoph.perger@spatial-focus.net>
2020-06-10 14:47:37 +0200

    NumSharp

--- src/MathPack/Basics.cs
+++ src/MathPack/Basics.cs
@@ -5,9 +5,18 @@
 namespace MathPack
 {
 	using System;
+	using NumSharp;
 
 	public static class Basics
 	{
+		public static int Numerics()
+		{
+			NDArray nd = np.full(5, 12); //[5, 5, 5 .. 5]
+			nd = np.zeros(12); //[0, 0, 0 .. 0]
+			nd = np.arange(12); //[0, 1, 2 .. 11]
+			return nd.ndim;
+		}
+
 		public static int Sum(int a, int b)
 		{
 			return a + b;
